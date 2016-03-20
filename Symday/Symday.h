b5c8a3cd10c870eceb3a2bfd7d8217bfe1ce9cb2
tnantoka <tnantoka@bornneet.com>
2016-03-20 22:18:35 +0900

    Create project

---
+++ Symday/Symday.h
@@ -0,0 +1,19 @@
+//
+//  Symday.h
+//  Symday
+//
+//  Created by Tatsuya Tobioka on 3/20/16.
+//  Copyright © 2016 Tatsuya Tobioka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Symday.
+FOUNDATION_EXPORT double SymdayVersionNumber;
+
+//! Project version string for Symday.
+FOUNDATION_EXPORT const unsigned char SymdayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Symday/PublicHeader.h>
+
+
